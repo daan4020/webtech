c87239edf075b4b029f97c326b355375a2d2634e
daan4020 <48174074+daan4020@users.noreply.github.com>
2023-04-12 17:16:06 +0200

    test
    put

--- process-form.php
+++ process-form.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
-  $to = 'Daan.devriespeize@gmail.com';
+  $to = 'daan.devriespeize@gmail.com';
   $subject = 'Nieuw contactformulierbericht van ' . $name;
   $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n" . 'Reply-To: ' . $email;
   mail($to, $subject, $message, $headers);
