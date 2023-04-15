6630c755007404f12a2a3b672320f4e3b0eefb00
daan4020 <48174074+daan4020@users.noreply.github.com>
2023-04-15 16:11:15 +0200

    new venvs folder

--- venvs/webtech/Scripts/activate.bat
+++ venvs/webtech/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\basbo\OneDrive\Bureaublad\School\webtech\venvs\webtech
+set VIRTUAL_ENV=C:\Users\daand\Desktop\webtech\venvs\webtech
 
 if not defined PROMPT set PROMPT=$P$G
 
